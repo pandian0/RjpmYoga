5f3de45c4c3f549fe3101b3b77128cde9ab62ca9
pandian0 <137278923+pandian0@users.noreply.github.com>
2023-06-21 15:28:15 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navbar from './Components/Navbar';
+import Slide from './Components/Slide'
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <Slide/>
+    </div>
+  );
+};
+
+export default App;
